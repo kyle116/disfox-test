02b6a7cae09de11f624a67f76e73e3f65a28e5e7
kyle liu <kyle116@yahoo.com>
2020-01-17 16:39:28 -0800

    updated heroku build

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const path = require('path');
 
 // Bodyparser Middleware
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -32,6 +32,6 @@ app.use('/reminders', reminders);
 const port = process.env.PORT || 3001;
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '../client/build/index.html'));
+	res.sendFile(path.join(__dirname + './client/build/index.html'));
 });
 app.listen(port, () => console.log(`Server started on port ${port}`));
